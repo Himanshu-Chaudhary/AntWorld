1d216a723801ea6ff4d1836ccfd3b77e8975d6b4
DebRez <rezanka@comcast.net>
2017-04-26 16:13:37 -0600

    Added a OurConstants class to put our constants in, keep them consolidated and easy to troubleshoot.
    Signed-off-by: DebRez <rezanka@comcast.net>

---
+++ src/antworld/client/OurConstants.java
@@ -0,0 +1,15 @@
+package antworld.client;
+
+/**
+ * Created by Deb Rezanka on 4/26/2017.
+ */
+public class OurConstants
+{
+  //These must add up to 1.0
+  public static double workersToSpawn = 0.5;
+  public static double explorersToSpawn = 0.5;
+  public static double warriorsToSpawn = 0.0;
+
+  //number of ants to spawn if debug is true in ArmyAntClient
+  public static int debugAntValue = 5;
+}
